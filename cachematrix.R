c8bcf85f17f33b8332b1153bc4f4b797fcebae78
Transamo <Transamo@users.noreply.github.com>
2015-07-24 11:51:22 -0400

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## Function caches invertible matrices and the inverses calculated by cacheSolve
+## Second functions calculates the inverses of the matrices and caches(stores) these inverses
+
+## makeCacheMatrix can be subsetted ...$... to store a matrix, get that matrix
+## or store an inverse and get that inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
+}
